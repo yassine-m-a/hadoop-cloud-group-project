4e4f689c40653a3862a23304ea56608afc7cf862
Yassine M <mernissiyassine@hotmail.fr>
2025-09-28 15:43:04 +0200

    Youhou my last commit

--- scripts/spark/pyspark_count_word.py
+++ scripts/spark/pyspark_count_word.py
@@ -5,7 +5,7 @@ conf = SparkConf().setAppName("SearchJeanmougin")
 sc = SparkContext(conf=conf)
 
 # RDD
-rdd = sc.textFile("hdfs:///user/ubuntu/frwiki.xml.bz2")
+rdd = sc.textFile("hdfs:///user/ubuntu/wikipedia_fr_dump_17Go.xml")
 
 # Filtrer les 
 results = rdd.filter(lambda line: "jeanmougin" in line.lower())
